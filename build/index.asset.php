2cd432f638ed8d2510c87659a6c68b12b01c0aeb
dnoloco <159826868+dnoloco@users.noreply.github.com>
2026-03-11 08:48:37 -0500

    Add files via upload

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('moment', 'react', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-date', 'wp-edit-post', 'wp-editor', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-server-side-render'), 'version' => 'dfa72e39b49545354d88');
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-develop',
+        'version' => 'dev-develop',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'e47c57ed672dadad1aa6e43278a851d6356845bf',
+        'name' => 'castos/seriously-simple-podcasting',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'castos/seriously-simple-podcasting' => array(
+            'pretty_version' => 'dev-develop',
+            'version' => 'dev-develop',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'e47c57ed672dadad1aa6e43278a851d6356845bf',
+            'dev_requirement' => false,
+        ),
+    ),
+);
